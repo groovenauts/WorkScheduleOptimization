5ba2785bc4df2cf52961e94d0ec75a66cb95d8c0
nakanori <n-nakamura@groovenauts.jp>
2018-09-06 19:37:18 +0900

    Add working days/hours columns in gunttchart

--- src/Gunttchart.jsx
+++ src/Gunttchart.jsx
@@ -16,9 +16,34 @@ class Gunttchart extends React.Component {
   render() {
     const { height, year, month, days, wdays, hours, staffs, events, onClickStaff } = this.props
     
-    let tableWidth = 0
-    let columns = _.map(days, (day, i) => {
-      return {
+    let columns = [{
+      key: "index",
+      dataIndex: "index",
+      title: <div className={"column-header"}>No</div>,
+      fixed: true,
+      width: 20,
+    }, {
+      key: "name",
+      dataIndex: "name",
+      title: <span className={"column-name"}>名前</span>,
+      fixed: true,
+      width: 120,
+    }, {
+      key: "sumDays",
+      dataIndex: "sumDays",
+      title: <div className={"column-header"}>日数</div>,
+      fixed: true,
+      width: 30,
+    }, {
+      key: "sumHours",
+      dataIndex: "sumHours",
+      title: <div className={"column-header"}>時間</div>,
+      fixed: true,
+      width: 30,
+    }]
+    let tableWidth = _.sumBy(columns, o => o.width)
+    _.each(days, (day, i) => {
+      columns.push({
         key: `day${day}`,
         title: <div className={"column-header"}>{i === 0 ? `${month} / ${day} (${wdays[i]})` : `${day} (${wdays[i]})`}</div>,
         children: _.map(hours, hour => {
@@ -30,26 +55,8 @@ class Gunttchart extends React.Component {
             width: columnWidth,
           }
         })
-      }
+      })
     })
-    columns.unshift({
-      key: "name",
-      dataIndex: "name",
-      title: <span className={"column-name"}>名前</span>,
-      fixed: true,
-      width: 160,
-    })
-    tableWidth += 160
-
-    columns.unshift({
-      key: "index",
-      dataIndex: "index",
-      title: <span className={"column-header"}>No</span>,
-      fixed: true,
-      width: 20,
-    })
-    tableWidth += 20
-
 
     const grouped = _.groupBy(events, 'staff_id')
     const dataSource = _.map(staffs, (staff, i) => {
@@ -58,21 +65,23 @@ class Gunttchart extends React.Component {
         index: <div className={"data-index"}>{i}</div>,
         name: <span className={"data-name"} onClick={() => onClickStaff(staff.id)}>{`${staff.first_name} ${staff.last_name}`}</span>,
         ...staff,
-        labeled: false,
       }
+      let sumDays = 0
+      let sumHours = 0
       _.each(days, day => {
+        let workingHours = 0
         _.each(hours, (hour, j) => {
           const hourStr = _.padStart(hour, 2, '0')
           const cell = moment(`${formatDate(year, month, day)} ${hourStr}:00:00`)
           let label = null
-          let workingHours = null
+          
           const onEvent = _.find(_.get(grouped, staff.id, []), event => {
             return cell.diff(event.start, 'hours') >= 0 && cell.diff(event.end, 'hours') < 0
           })
           if (onEvent) {
             const duration = moment.duration(onEvent.end.diff(onEvent.start))
-            workingHours = duration.as('hours')
-            if (!data.labeled) {
+            if (workingHours === 0) {
+              workingHours = duration.as('hours')
               label = (
                 <Popover
                   content={
@@ -88,7 +97,6 @@ class Gunttchart extends React.Component {
                   </p>
                 </Popover>
               )
-              data.labeled = true
             }
           }
           data[`day${day}hour${hour}`] = (onEvent &&
@@ -97,7 +105,13 @@ class Gunttchart extends React.Component {
             </div>
           )
         })
+        sumHours += workingHours
+        if (workingHours > 0) {
+          sumDays += 1
+        }
       })
+      data['sumDays'] = <span className="to-right">{sumDays}</span>
+      data['sumHours'] = <span className="to-right">{sumHours}</span>
       return data
     })
 
