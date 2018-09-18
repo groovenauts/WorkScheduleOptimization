656373b392c472e1cecc5c2cc8e67ec1b9063438
nakanori <n-nakamura@groovenauts.jp>
2018-09-19 02:41:19 +0900

    Fix non display the storybook

--- stories/Loading.stories.js
+++ stories/Loading.stories.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { storiesOf, addDecorator } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import moment from 'moment'
+import { Icon } from 'antd';
 
 import { antdDecorator } from './Decorator'
 import Mask from '../src/Mask'
@@ -9,5 +10,6 @@ import Loading from '../src/Loading'
 
 storiesOf('Mask/Loading', module)
   .addDecorator(getStory => antdDecorator(getStory))
-  .add("normal", () => <Mask visible={true}><Loading /></Mask>)
-  .add("with text", () => <Mask visible={true}><Loading text={"Loading..."} /></Mask>)
+  .add("normal", () => <Mask visible={true}><Loading visible={true}/></Mask>)
+  .add("with text", () => <Mask visible={true}><Loading visible={true} text={"Loading..."} /></Mask>)
+  .add("with children", () => <Mask visible={true}><Icon type="loading" theme="outlined" style={{color: '#060077', fontSize: 34, display: 'block'}}/></Mask>)
