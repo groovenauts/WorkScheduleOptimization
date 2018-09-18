11595bd8472d05ad2ca39db6382a62d9348ac00b
nakanori <n-nakamura@groovenauts.jp>
2018-09-19 02:43:07 +0900

    Make it loading-icon while re-render

--- src/Switch.jsx
+++ src/Switch.jsx
@@ -6,12 +6,34 @@ import {
 import { VIEW_MODE } from './reducers/tab2'
 
 class Switch extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: null, // VIEW_MODE.PEOPLE or VIEW_MODE.TIME
+    }
+  }
   shouldComponentUpdate(nextProps, nextState) {
+    const { loading } = this.state
     const { height, value } = this.props
-    return value !== nextProps.value || height !== nextProps.height
+    return (
+      value !== nextProps.value ||
+      height !== nextProps.height ||
+      loading !== nextState.loading
+    )
+  }
+  handleChange(value) {
+    this.setState({loading: value}, () => {
+      setTimeout(() => {
+        this.props.onChange(value)
+      }, 100)
+      setTimeout(() => {
+        this.setState({loading: null})
+      }, 3000)
+    })
   }
   render() {
-    const { height, size, value, onChange } = this.props
+    const { loading } = this.state
+    const { height, size, value } = this.props
     let style = {}
     if (_.isNumber(height)) {
       style.height = height
@@ -28,16 +50,16 @@ class Switch extends React.Component {
     return (
       <div className="switch slideDown" style={style}>
         <div style={style} className={value === VIEW_MODE.PEOPLE ? 'active':''}>
-          <Icon type="team" theme="outlined" style={iconStyle} onClick={e => {
-            if (value !== VIEW_MODE.PEOPLE) {
-              onChange(VIEW_MODE.PEOPLE)
+          <Icon type={loading === VIEW_MODE.PEOPLE ? 'loading':'team'} theme="outlined" style={iconStyle} onClick={e => {
+            if (!loading && value !== VIEW_MODE.PEOPLE) {
+              this.handleChange(VIEW_MODE.PEOPLE)
             }
           }}/>
         </div>
         <div style={style} className={value === VIEW_MODE.TIME ? 'active':''}>
-          <Icon type="clock-circle" theme="outlined" style={iconStyle} onClick={e => {
-            if (value !== VIEW_MODE.TIME) {
-              onChange(VIEW_MODE.TIME)
+          <Icon type={loading === VIEW_MODE.TIME ? 'loading':'clock-circle'} theme="outlined" style={iconStyle} onClick={e => {
+            if (!loading && value !== VIEW_MODE.TIME) {
+              this.handleChange(VIEW_MODE.TIME)
             }
           }}/>
         </div>
